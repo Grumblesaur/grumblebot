e7db5b451e07775e419872e98eba15ad4566c66b
James Murphy <jmurphy4@mail.umw.edu>
2015-09-16 13:50:38 -0400

    Broken into functions.

--- grumblebot.py
+++ grumblebot.py
@@ -3,6 +3,24 @@ import sys
 import time
 import random
 
+def botwatch(data):	
+	bottracker = 0
+	data = data.lower()
+	if data.find("taiya") != -1 or data.find("goatbot") != -1:
+		bottracker += 1
+		if bottracker % 25 == 0:
+			irc.send("PRIVMSG " + channel + " :Do not feed the bots.\r\n")
+
+def rollwatch(data):
+		data = data.split()
+		cap = 10
+		for item in data:
+			if item.isdigit() == True:
+				cap = int(item)
+				break
+		value = random.randrange(cap)
+		irc.send("PRIVMSG " + channel + " " + str(value) + "\r\n")
+	
 nick = "grumblebot"
 server = "irc.gamesurge.net"
 port = 6667
@@ -32,22 +50,15 @@ while True:
 		connected = True
 		irc.send("PRIVMSG " + channel + " :Hello LT IRC!\r\n")
 	
-	bottracker = 0
-	data = data.lower()
-	if data.find("taiya") != -1 or data.find("goatbot") != -1:
-		bottracker += 1
-		if bottracker % 25 == 0:
-			irc.send("PRIVMSG " + channel + " :Do not feed the bots.\r\n")
-			
+	# clean the data stream for botwatch and rollwatch
+	data.lower()
+	for char in data:
+		if char in "?.!/;:,()[]{}#$%^&*@!":
+			data = data.replace(char,"")
+	
+	botwatch(data)			
+	
 	if data.find("grumblebot roll") != -1:
-		data = data.split()
-		cap = 10
-		for item in data:
-			if item.isdigit() == True:
-				cap = int(item)
-				break
-		value = random.randrange(cap)
-		irc.send("PRIVMSG " + channel + " " + str(value) + "\r\n")
+		rollwatch(data)
+
 	
-	
-				
