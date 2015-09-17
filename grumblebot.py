f46477f360dfca7fd15c963d2f281d9bbb9743e8
James Murphy <jmurphy4@mail.umw.edu>
2015-09-16 21:38:43 -0400

    Added stuff.

--- grumblebot.py
+++ grumblebot.py
@@ -2,55 +2,74 @@ import socket
 import sys
 import time
 import random
+import os
+
+bottracker = 0
+bots = ["taiya", "fwipbot", "goatbot"]
+nick = "grumblebot"
+serv = "irc.gamesurge.net"
+port = 6667
+chan = "#limittheory"
+
 
 def botwatch(data):	
-	bottracker = 0
+	global bottracker
+	global bots
 	data = data.lower()
-	if data.find("taiya") != -1 or data.find("goatbot") != -1:
-		bottracker += 1
-		if bottracker % 25 == 0:
-			irc.send("PRIVMSG " + channel + " :Do not feed the bots.\r\n")
+	for bot in bots:
+		if bot in data:
+			bottracker += 1
+			if bottracker % 25 == 0:
+				irc.send("PRIVMSG " + chan + " :Do not feed the bots.\r\n")
 
 def rollwatch(data):
-		data = data.split()
-		cap = 10
-		for item in data:
-			if item.isdigit() == True:
-				cap = int(item)
-				break
-		value = random.randrange(cap)
-		irc.send("PRIVMSG " + channel + " " + str(value) + "\r\n")
-	
-nick = "grumblebot"
-server = "irc.gamesurge.net"
-port = 6667
-channel = "#limittheory"
+	data = data.split()
+	cap = 10
+	for item in data:
+		if item.isdigit() == True:
+			cap = int(item)
+			break
+	if cap < 1:
+		irc.send("PRIVMSG " + chan + "Invalid RNG cap!\r\n")
+		irc.send("PRIVMSG " + chan + "Taiya, tell Grumblesaur to punish them.\r\n")
+		return
+	value = random.randrange(cap)
+	irc.send("PRIVMSG " + chan + " " + str(value) + "\r\n")
 
+def quitwatch(data):
+	if "grumblebot quit" in data:
+		os.exit()
+
+def greetwatch(data):
+	if "hi grumblebot" in data:
+		irc.send("PRIVMSG " + chan + "Hello!\r\n")
+	if "hello grumblebot" in data:
+		irc.send("PRIVMSG " + chan + "Hello!\r\n")
+	
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-irc.connect((server, port))
+irc.connect((serv, port))
 
 irc.recv(4096)
 irc.send("NICK " + nick + "\r\n")
 irc.send("USER " + nick + " " + nick + " " + nick + ":Grumblesaur IRC\r\n")
-irc.send("JOIN " + channel + "\r\n")
-irc.send("PRIVMSG " + channel + " :Hello.\r\n")
+irc.send("JOIN " + chan + "\r\n")
+irc.send("PRIVMSG " + chan + " :Hello.\r\n")
 
 connected = False
 
 while True:
-	data = irc.recv(4096)
+	data = irc.recv(2048)
 	sys.stdout.write(str(data))
 	if data.find("PING") != -1:
 		irc.send("PONG " + data.split()[1] + "\r\n")
 	
 	if connected == False:
-		irc.send("JOIN " + channel + "\r\n")
+		irc.send("JOIN " + chan + "\r\n")
 	
 	if "@Bele" in data:
 		connected = True
-		irc.send("PRIVMSG " + channel + " :Hello LT IRC!\r\n")
+		irc.send("PRIVMSG " + chan + " :Hello LT IRC!\r\n")
 	
-	# clean the data stream for botwatch and rollwatch
 	data.lower()
 	for char in data:
 		if char in "?.!/;:,()[]{}#$%^&*@!":
@@ -60,5 +79,8 @@ while True:
 	
 	if data.find("grumblebot roll") != -1:
 		rollwatch(data)
-
+	
+	greetwatch(data)
+	quitwatch(data)
+	
 	
